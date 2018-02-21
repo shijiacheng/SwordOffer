8cde9f6ca714b1f3d951b763eb37ebccbdda7cae
shijiacheng <iot_shijiacheng@163.com>
2018-02-21 08:38:44 +0800

    变态跳台阶

--- src/com/shijc/nowcoder/JumpFloorIISolution.java
+++ src/com/shijc/nowcoder/JumpFloorIISolution.java
@@ -1,6 +1,7 @@
 package com.shijc.nowcoder;
 
 /**
+ * 变态跳台阶
  * 题目描述
  * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
  * ================思路=================
@@ -42,7 +43,16 @@ preNum = result;
 return result;
  */
 public class JumpFloorIISolution {
-//    public int JumpFloorII(int target) {
-//
-//    }
+    public int JumpFloorII(int target) {
+        if (target == 1){
+            return 1;
+        }
+        int result = 0;
+        int preNum = 1;
+        for (int i = 2; i <= target ; i++) {
+            result = 2 * preNum;
+            preNum = result;
+        }
+        return result;
+    }
 }
