8904db4b9703db17288ca191c8f8131c471feff7
shijiacheng <iot_shijiacheng@163.com>
2018-02-23 19:14:00 +0800

    树的子结构

---
+++ src/com/shijc/nowcoder/SubstructureInTreeSolution.java
@@ -0,0 +1,42 @@
+package com.shijc.nowcoder;
+
+/**
+ * 树的子结构
+ * 题目描述
+ * 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
+ *
+ * @author shijiacheng
+ * @date 2018/2/23
+ */
+public class SubstructureInTreeSolution {
+    public boolean HasSubtree(TreeNode root1, TreeNode root2) {
+        boolean result = false;
+        if (root1 != null && root2 != null) {
+            if (root1.val == root2.val) {
+                result = isTree1HasTree2(root1, root2);
+            }
+            if (!result) {
+                result = HasSubtree(root1.left, root2);
+            }
+            if (!result) {
+                result = HasSubtree(root1.right, root2);
+            }
+        }
+
+        return result;
+    }
+
+    public boolean isTree1HasTree2(TreeNode root1, TreeNode root2) {
+        if (root2 == null) {
+            return true;
+        }
+        if (root1 == null) {
+            return false;
+        }
+        if (root1.val != root2.val) {
+            return false;
+        }
+        return isTree1HasTree2(root1.left, root2.left)
+                && isTree1HasTree2(root1.right, root2.right);
+    }
+}
