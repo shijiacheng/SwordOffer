08511709db282809955dbf0fde3d1ef72b163d16
shijiacheng <iot_shijiacheng@163.com>
2018-02-23 19:10:10 +0800

    树

---
+++ src/com/shijc/nowcoder/TreeNode.java
@@ -0,0 +1,16 @@
+package com.shijc.nowcoder;
+
+/**
+ * @author shijiacheng
+ */
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
