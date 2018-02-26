67700daaf0ebcddf271342216f28562b35f8a202
shijiacheng <iot_shijiacheng@163.com>
2018-02-26 20:21:54 +0800

    复杂链表类型

---
+++ src/com/shijc/nowcoder/RandomListNode.java
@@ -0,0 +1,14 @@
+package com.shijc.nowcoder;
+
+/**
+ * Created by shijiacheng on 2018/2/26.
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
