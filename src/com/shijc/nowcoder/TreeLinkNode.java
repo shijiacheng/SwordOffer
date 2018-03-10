7a3147f4dd97edda553c44f98ca03534c633efe6
shijiacheng <iot_shijiacheng@163.com>
2018-03-10 12:53:37 +0800

    treeLinkNode类

---
+++ src/com/shijc/nowcoder/TreeLinkNode.java
@@ -0,0 +1,15 @@
+package com.shijc.nowcoder;
+
+/**
+ * Created by shijiacheng on 2018/3/10.
+ */
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
